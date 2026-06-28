a9e482d5957b24eceab6a12ef1ad4ae43f62427d
aishwarya-studio <aishugunjurge@gmail.com>
2026-06-28 18:00:29 +0530

    Add files via upload

---
+++ js/rangoli-images.js
@@ -0,0 +1,24 @@
+const rangoliImages = [];
+const mehendiImages = [];
+const drawingImages = [];
+const decorationImages = [];
+
+/* Rangoli Images */
+for(let i=1; i<=35; i++){
+    rangoliImages.push(`Media/Img/Rangoli/${i}.jpeg`);
+}
+
+/* Mehendi Images */
+for(let i=1; i<=87; i++){
+    mehendiImages.push(`Media/Img/Mehndi/${i}.jpeg`);
+}
+
+/* Drawing Images */
+for(let i=1; i<=25; i++){
+    drawingImages.push(`Media/Img/Drawing/${i}.jpeg`);
+}
+
+/* Decorations Images */
+for(let i=1; i<=64; i++){
+    decorationImages.push(`Media/Img/Decorations/${i}.jpeg`);
+}
